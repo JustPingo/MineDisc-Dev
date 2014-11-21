b03d75a20538bfbbe98a364b0ac2f0b8afdb258b
JustPingo <theodegio@yahoo.fr>
2014-11-21 18:23:23 +0100

    First commit
    Empty mod.

---
+++ pingo/minedisc/common/MineDisc.java
@@ -0,0 +1,21 @@
+package pingo.minedisc.common;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = MineDisc.MODID, version = MineDisc.VERSION, name = MineDisc.NAME)
+public class MineDisc
+{
+    public static final String MODID = "minedisc";
+    public static final String NAME = "MineDisc";
+    public static final String VERSION = "0.1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    	
+    	
+    }
+}
